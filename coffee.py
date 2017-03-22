61b1fed6cb04accaa71afec0366c24f75b1a8bc8
dpebert7 <dpebert7@gmail.com>
2017-03-22 15:49:22 -0500

    add mikaela

--- coffee.py
+++ coffee.py
@@ -22,7 +22,9 @@ minorLocator = AutoMinorLocator(3)
 
 parker = pd.Series([0.5]*88)
 
-mary = pd.Series([3.0]*6+[6.0]*32+[6.01]*10+[6.02]*10+[6.05]*10+[6.09]*10+[6.10]*2+[6.11]*2+[6.12]*2+[6.13]*2+[6.14]+6.15)
+time = pd.Series([i for i in range(1,89)])
+
+mary = pd.Series([0.1]*7+[6.0]*11+[5.9]*10+[6.0]*10+[5.9]*10+[6.0]*10+[5.9]*10+[6.0]*9+[6.02]*3+[6.05]*2+[6.08]+[6.11]+[6.12]+[6.13]+[6.14]+[6.15])
 
 david = pd.Series([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
                     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
@@ -42,16 +44,8 @@ joseph = pd.Series([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0,
                     3.0, 1.0, 3.0, 1.0, 4.5, 3.0, 2.0, 3.25, 1.5, #np.nan, np.nan, 
                     3.0, 1.0, 2.75, 1.5, 2.5, 1.5, 2.5])
 
-"""    
-mikaela =pd.Series([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-                    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-                    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-                    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-                    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-                    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-                    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-                    1.0, 1.0, 1.0, 1.0])
-"""
+mikaela = pd.Series([3.0]*6+[5.9]*12+[6.0]*10+[5.9]*10+[6.0]*10+[5.9]*10+[6.0]*10+[5.9]*5+[0.0]*15)
+
 
 adam = pd.Series([  0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05,
                     0.08, 0.12, 0.19, 0.36, 0.57, 0.72, 0.98, 1.22, 1.45, 1.66, 1.88, 1.99,
@@ -67,7 +61,7 @@ adam = pd.Series([  0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05,
 xlabels = ["Aug '15", "Nov '15", "Feb '16", "May '16", "Aug '16", "Nov '16", "Feb '17", "May '17"]
 ylabels = ["0","1","2","3","4","5","6+",""]            
 
-plt.plot(x, y, 'ro')
+#plt.plot(x, y, 'ro')
 # You can specify a rotation for the tick labels in degrees or with keywords.
 
 
@@ -75,17 +69,19 @@ threshold = 78
 below = time <= threshold
 above = time >= threshold
 
-fig = plt.figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
+fig = plt.figure(num=None, figsize=(15, 6), dpi=180, facecolor='w', edgecolor='k')
 with plt.xkcd():
     ax = plt.subplot(111)
-    ax.plot(time[below], mary[below], 'b-')
+    ax.plot(time[below], mary[below], 'k-')
+    ax.plot(time[below], mikaela[below], 'r-')
     ax.plot(time[below], adam[below], 'g-')
-    ax.plot(time[below], joseph[below], 'r-')
+    ax.plot(time[below], joseph[below], 'b-')
     ax.plot(time[below], parker[below], 'c-')
     ax.plot(time[below], david[below], 'm-')
-    ax.plot(time[above], mary[above], 'b:')
+    ax.plot(time[above], mary[above], 'k:')
+    ax.plot(time[above], mikaela[above], 'r:')
     ax.plot(time[above], adam[above], 'g:')
-    ax.plot(time[above], joseph[above], 'r:')
+    ax.plot(time[above], joseph[above], 'b:')
     ax.plot(time[above], parker[above], 'c:')
     ax.plot(time[above], david[above], 'm:')
     #ax.text(81.6, 2.9, r'??', fontsize = 16, color = 'red')
@@ -103,7 +99,7 @@ with plt.xkcd():
     plt.title("GTA Coffee consumption")
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width*0.9, box.height])
-    ax.legend(['Mary', 'Adam', 'Joseph', 'Parker', 'David'], bbox_to_anchor=(1.2, .75))
+    ax.legend(['Mary', 'Mikaela','Adam', 'Joseph', 'Parker', 'David'], bbox_to_anchor=(1.2, .75))
     plt.savefig('coffee.png')
 
 
